da3baf1b9a6c067959cf19d1c21a0fb84802658a
Steyntjeh <steyn.ruesink@gmail.com>
2024-04-09 09:41:08 +0100

    database updat44

--- netwerken_toets_2024/config/db_config.php
+++ netwerken_toets_2024/config/db_config.php
@@ -3,7 +3,7 @@
 $dbhost = 'localhost';
 $dbuser = 'net24sruesink_net_user_2024	';
 $dbpass = 'netwerken2024!';
-$dbname = 'net24sruesinkg_netwerken_2024';
+$dbname = 'net24sruesink_netwerken_2024';
 
 $conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 if($conn->connect_error)
